ed3c513a87c18ba0a8923d761d2c25836224e01c
Eya Ngo Ndjip <rosendjip@MacBook-Air-de-Rose.local>
2023-01-22 15:27:05 +0100

    Change app text

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>
-        {" "}
-        Hello Eya and Mamie and Ngo Ndjip Liamidi and our big family again
-      </h1>
+      <h1> Hello Eya and all my Family</h1>
     </div>
   );
 }
